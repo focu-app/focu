36b1780ecf2716a3036a93c93c2cf2cdcc32be9d
Martin Buur <martinbuurman@pm.me>
2024-10-14 10:01:22 +0400

    chore: delete landing page

--- apps/landing-page/data/config/headerNavLinks.ts
+++
@@ -1,9 +0,0 @@
-export const headerNavLinks: Array<{
-  href: string;
-  title: string;
-}> = [
-  { href: '/', title: 'Home' },
-  { href: '/pricing', title: 'Pricing' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/about', title: 'About' },
-];
