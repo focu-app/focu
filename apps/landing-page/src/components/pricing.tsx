6d535b3874ca793f229db497a6323c5c97af876d
Martin Buur <martinbuurman@pm.me>
2024-10-15 13:00:33 +0400

    chore: update pricing

--- apps/landing-page/src/components/pricing.tsx
+++ apps/landing-page/src/components/pricing.tsx
@@ -17,23 +17,23 @@ const tiers: Tier[] = [
   {
     name: 'Individual',
     id: 'tier-individual',
-    href: '#',
+    href: 'https://focu.lemonsqueezy.com/buy/8bad2cd2-cf2e-4fed-af07-4acf8a520fab',
     price: '$0',
     discount: '$19',
-    description: 'For when you only have one device',
+    description: 'No subscription, free updates',
     features: ['1 Device', 'Local AI', 'Focus Page', 'Pomodoro Timer', 'Keyboard Shortcuts'],
-    mostPopular: false,
-  },
-  {
-    name: 'Pro',
-    id: 'tier-pro',
-    href: '#',
-    price: '$0',
-    discount: '$29',
-    description: 'For when you have multiple devices',
-    features: ['3 Devices', 'Local AI', 'Focus Page', 'Pomodoro Timer', 'Keyboard Shortcuts'],
     mostPopular: true,
   },
+  // {
+  //   name: 'Pro',
+  //   id: 'tier-pro',
+  //   href: 'https://focu.lemonsqueezy.com/buy/8bad2cd2-cf2e-4fed-af07-4acf8a520fab',
+  //   price: '$0',
+  //   discount: '$29',
+  //   description: 'For when you have multiple devices',
+  //   features: ['3 Devices', 'Local AI', 'Focus Page', 'Pomodoro Timer', 'Keyboard Shortcuts'],
+  //   mostPopular: true,
+  // },
 ]
 
 function classNames(...classes: string[]): string {
@@ -57,7 +57,7 @@ export function Pricing() {
           Choose the perfect plan for individuals and teams. No hidden fees.
         </p>
         
-        <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1 gap-8 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+        <div className="isolate mx-auto mt-10 grid max-w-md grid-cols-1">
           {tiers.map((tier) => (
             <div
               key={tier.id}
