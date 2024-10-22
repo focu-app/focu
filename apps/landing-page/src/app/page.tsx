e521118cb61cfc1732ad051dccfb95b07c502558
Martin Buur <martinbuurman@pm.me>
2024-10-22 08:57:29 +0400

    chore: replace download button

--- apps/landing-page/src/app/page.tsx
+++ apps/landing-page/src/app/page.tsx
@@ -175,12 +175,7 @@ export default function Example() {
               Help us improve and pay what you want during early access.
             </p>
             <div className="mt-10 flex items-center justify-center gap-x-6">
-              <a
-                href="https://focu.lemonsqueezy.com/buy/8bad2cd2-cf2e-4fed-af07-4acf8a520fab"
-                className="rounded-md bg-white px-3.5 py-2.5 text-sm font-semibold text-gray-900 shadow-sm hover:bg-gray-100 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white"
-              >
-                Download now
-              </a>
+              <DownloadButton />
             </div>
           </div>
         </div>
