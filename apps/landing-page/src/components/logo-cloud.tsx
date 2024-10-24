e2f15c377881873baf52df0191bc94a823156dc4
Martin Buur <martinbuurman@pm.me>
2024-10-24 16:10:00 +0400

    chore: better copywriting

--- apps/landing-page/src/components/logo-cloud.tsx
+++ apps/landing-page/src/components/logo-cloud.tsx
@@ -8,9 +8,12 @@ export function LogoCloud() {
   return (
     <div className="bg-gray-900 py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <h2 className="text-center text-lg font-semibold leading-8 text-white">
-          Powered by the latest open-source LLM software
+        <h2 className="text-4xl text-center font-bold tracking-tight text-white sm:text-5xl">
+          Powered by open-source software
         </h2>
+        <p className="mt-4 text-center text-pretty text-sm text-gray-400 sm:text-xl/8">
+          Focu does not require you to stay online, everything runs locally on your own Mac.
+        </p>
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10">
           <Image src={ollamaLogo} alt="Ollama" width={125/2}/>
           <Image src={geminiLogo} alt="Gemini" width={125} />
