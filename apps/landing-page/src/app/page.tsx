44a4f22698d8edd989ab5770f333896f2a27189c
Martin Buur <martinbuurman@pm.me>
2024-10-24 15:38:09 +0400

    chore: different screenshot

--- apps/landing-page/src/app/page.tsx
+++ apps/landing-page/src/app/page.tsx
@@ -9,6 +9,7 @@ import {
 } from '@heroicons/react/20/solid'
 import Image from 'next/image'
 import morningIntention from '@/images/morning-intention-2.png'
+import homescreen from '@/images/homescreen.png'
 import logo from '../images/logo.png'
 import { Video } from '@/components/video'
 import { Suspense } from 'react'
@@ -81,11 +82,11 @@ export default function Example() {
                 </a>
               </div>
             </div>
-            <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-32">
+            <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mr-0 lg:mt-0 lg:max-w-none lg:flex-none xl:ml-16">
               <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
                 <Image
                   alt="App screenshot"
-                  src={morningIntention}
+                  src={homescreen}
                   width={1020}
                   height={778}
                   className="w-[1020px] rounded-md bg-white/5 shadow-1xl ring-1 ring-white/10"
