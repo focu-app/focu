c97a1ec1252f4516b4c4f152ed0e4c2eacb26ac2
Martin Buur <martinbuurman@pm.me>
2024-10-25 10:03:11 +0400

    fix: close mobile menu on click

--- apps/landing-page/src/components/header.tsx
+++ apps/landing-page/src/components/header.tsx
@@ -68,6 +68,7 @@ export function Header() {
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-white hover:bg-gray-800"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
                     {item.name}
                   </a>
