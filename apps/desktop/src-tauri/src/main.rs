abd1e66fe64cf8c27dc0baa60ab16e8edcc32702
Martin Buur <martinbuurman@pm.me>
2024-11-07 09:13:29 +0400

    chore: fix tray window

--- apps/desktop/src-tauri/src/main.rs
+++ apps/desktop/src-tauri/src/main.rs
@@ -7,10 +7,10 @@ use std::os::unix::fs::PermissionsExt;
 use std::path::PathBuf;
 use std::process::Command;
 
-use tauri::tray::TrayIconBuilder;
-use tauri::{
-    AppHandle, Manager, RunEvent, TitleBarStyle, WebviewUrl, WebviewWindowBuilder, WindowEvent,
-};
+use tauri::menu::{MenuBuilder, MenuItemBuilder};
+use tauri::tray::{MouseButton, MouseButtonState, TrayIconBuilder, TrayIconEvent};
+use tauri::{Manager, RunEvent, TitleBarStyle, WebviewUrl, WebviewWindowBuilder, WindowEvent};
+use tauri_plugin_positioner::{Position, WindowExt};
 
 use cocoa::appkit::{NSApp, NSImage};
 use cocoa::base::{id, nil};
@@ -43,30 +43,30 @@ pub fn start_watchdog(parent_pid: u32, ollama_pid: u32) -> Result<(), std::io::E
     Ok(())
 }
 
-// fn create_tray_window(app: &tauri::AppHandle) -> Result<Window, tauri::Error> {
-//     let window = WebviewWindowBuilder::new(app, "tray", WebviewUrl::App("/tray".into()))
-//         .inner_size(300.0, 250.0)
-//         .decorations(true)
-//         .resizable(false)
-//         .closable(false)
-//         .minimizable(false)
-//         .focused(true)
-//         .always_on_top(true)
-//         .hidden_title(true)
-//         .title_bar_style(TitleBarStyle::Overlay)
-//         .visible(false) // Start hidden
-//         .build()?;
+fn create_tray_window(app_handle: &tauri::AppHandle) -> Result<tauri::WebviewWindow, tauri::Error> {
+    let window = WebviewWindowBuilder::new(app_handle, "tray", WebviewUrl::App("/tray".into()))
+        .inner_size(300.0, 250.0)
+        .decorations(true)
+        .resizable(false)
+        .closable(false)
+        .minimizable(false)
+        .focused(true)
+        .always_on_top(true)
+        .hidden_title(true)
+        .title_bar_style(TitleBarStyle::Overlay)
+        .visible(false) // Start hidden
+        .build()?;
 
-//     let window_clone = window.clone();
-//     window.on_window_event(move |event| {
-//         if let WindowEvent::Focused(focused) = event {
-//             if !focused {
-//                 let _ = window_clone.hide();
-//             }
-//         }
-//     });
-//     Ok(window)
-// }
+    let window_clone = window.clone();
+    window.on_window_event(move |event| {
+        if let WindowEvent::Focused(focused) = event {
+            if !focused {
+                let _ = window_clone.hide();
+            }
+        }
+    });
+    Ok(window)
+}
 
 #[tauri::command]
 fn start_ollama() -> Result<u32, String> {
@@ -200,7 +200,7 @@ fn main() {
 
     #[allow(unused_mut)]
     let mut app = tauri::Builder::default()
-        // .plugin(tauri_plugin_positioner::init())
+        .plugin(tauri_plugin_positioner::init())
         // .system_tray(system_tray)
         // .on_system_tray_event(|app, event| {
         //     tauri_plugin_positioner::on_tray_event(app, &event);
@@ -235,7 +235,7 @@ fn main() {
         // })
         .plugin(tauri_plugin_global_shortcut::Builder::default().build())
         .setup(move |app| {
-            // create_tray_window(&app.handle())?;
+            create_tray_window(app.handle())?;
 
             // Check if onboarding is needed
             if !is_onboarding_completed(&app.handle()) {
@@ -252,6 +252,7 @@ fn main() {
                 .title("Welcome")
                 .inner_size(800.0, 600.0)
                 .center()
+                .focused(true)
                 .build()?;
             } else {
                 if let Some(main_window) = app.get_webview_window("main") {
@@ -270,6 +271,54 @@ fn main() {
                 }
                 Err(error) => eprintln!("Failed to start Ollama: {}", error),
             }
+
+            let quit = MenuItemBuilder::with_id("quit", "Quit").build(app)?;
+            let menu = MenuBuilder::new(app).items(&[&quit]).build()?;
+            let tray = app.handle().tray_by_id("main_tray").unwrap();
+
+            tray.set_menu(Some(menu))?;
+
+            tray.on_tray_icon_event(|tray, event| {
+                tauri_plugin_positioner::on_tray_event(tray.app_handle(), &event);
+
+                match event {
+                    TrayIconEvent::Click {
+                        button: MouseButton::Left,
+                        button_state: MouseButtonState::Up,
+                        ..
+                    } => {
+                        println!("left click pressed and released");
+                        let app_handle = tray.app_handle();
+                        if let Some(tray_window) = app_handle.get_webview_window("tray") {
+                            if !tray_window.is_visible().unwrap_or(false) {
+                                let _ = tray_window.move_window(Position::TrayCenter);
+                                let _ = tray_window.show();
+                                let _ = tray_window.set_focus();
+                            } else {
+                                let _ = tray_window.hide();
+                            }
+                        }
+                    }
+                    _ => {}
+                }
+            });
+
+            // let tray = TrayIconBuilder::new().build(app)?;
+            // tray.on_tray_icon_event(|tray, event| match event {
+            //     SystemTrayEvent::LeftClick { .. } => {
+            //         println!("left click");
+            //         if let Some(tray) = app.get_window("tray") {
+            //             if !tray.is_visible().unwrap_or(false) {
+            //                 let _ = tray.move_window(Position::TrayCenter);
+            //                 let _ = tray.show();
+            //                 let _ = tray.set_focus();
+            //             } else {
+            //                 let _ = tray.hide();
+            //             }
+            //         }
+            //     }
+            //     _ => {}
+            // });
 
             Ok(())
         })
