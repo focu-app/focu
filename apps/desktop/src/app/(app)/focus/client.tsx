bf5e374e85a38f34a825c74c8f01bf50491126ec
Martin Buur <martinbuurman@pm.me>
2024-11-11 10:48:13 +0400

    chore: slightly better styling

--- apps/desktop/src/app/(app)/focus/client.tsx
+++ apps/desktop/src/app/(app)/focus/client.tsx
@@ -9,9 +9,9 @@ import { ScrollArea } from "@repo/ui/components/ui/scroll-area";
 
 export default function FocusClient() {
   return (
-    <div className="flex flex-col h-full bg-background/20">
+    <div className="flex flex-col h-full">
       <DateNavigationHeader />
-      <ScrollArea className="flex flex-col h-full bg-background/40 dark:bg-background/70">
+      <ScrollArea className="flex flex-col h-full bg-background/80 dark:bg-background/50">
         <div className="flex h-full w-full">
           <div className="flex flex-col max-w-2xl w-full mx-auto p-4 gap-4">
             <PomodoroTimer />
