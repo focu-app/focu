9e7829320ed7c83e1db893f7b29282b13a9ebd60
Martin Buur <martinbuurman@pm.me>
2024-12-02 11:45:28 +0400

    feature: add senja to pricing page

--- apps/landing-page/src/app/pricing/page.tsx
+++ apps/landing-page/src/app/pricing/page.tsx
@@ -1,5 +1,6 @@
 import { BottomCTA } from "@/components/bottom-cta";
 import { Pricing } from "@/components/pricing";
+import SenjaTestimonials from "@/components/senja-testimonials";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -12,6 +13,10 @@ export default function FeaturePage() {
     <div className="mx-auto max-w-7xl px-6 pb-24 pt-10 sm:pb-40 lg:px-8 lg:pt-20">
       <div className="flex flex-col gap-16 pt-8">
         <Pricing />
+      </div>
+      <div className="py-24 max-w-screen-lg mx-auto relative">
+        <SenjaTestimonials />
+        <div className="absolute bottom-0 left-0 right-0 h-[140px] bg-gray-900" />
       </div>
       <BottomCTA />
     </div>
