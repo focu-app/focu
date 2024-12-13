243095d23986af7e253df596d71922d83be12903
Martin Buur <martinbuurman@pm.me>
2024-12-13 08:45:02 +0200

    fix: sitemap

--- apps/landing-page/src/app/sitemap.ts
+++ apps/landing-page/src/app/sitemap.ts
@@ -1,9 +1,14 @@
 import type { MetadataRoute } from 'next'
-import { allFeatures } from "content-collections";
+import { allFeatures, allLegals } from "content-collections";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const features = allFeatures.map((post) => ({
     url: `https://focu.app/feature/${post.slug}`,
+    lastModified: new Date(post.publishedAt).toISOString().split("T")[0],
+  }));
+
+  const legals = allLegals.map((post) => ({
+    url: `https://focu.app/${post.slug}`,
     lastModified: new Date(post.publishedAt).toISOString().split("T")[0],
   }));
 
@@ -16,5 +21,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
   return [
     ...features,
     ...routes,
+    ...legals,
   ]
 }
