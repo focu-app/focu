343f7d3d9e2a8873fe3f15e2bb8f9ad3c834fa9f
Martin Buur <martinbuurman@pm.me>
2025-01-25 15:49:09 +0400

    fix: ignore analytics script on deployment URLs

--- apps/landing-page/src/app/CustomAnalytics.tsx
+++ apps/landing-page/src/app/CustomAnalytics.tsx
@@ -3,8 +3,14 @@ import Script from "next/script";
 export const CustomAnalytics = () => {
   const isDev =
     process.env.NEXT_PUBLIC_VERCEL_ENV === "development" ||
-    process.env.NEXT_PUBLIC_VERCEL_ENV === "preview" ||
-    window.location.hostname.endsWith("vercel.app");
+    process.env.NEXT_PUBLIC_VERCEL_ENV === "preview";
+
+  if (
+    typeof window !== "undefined" &&
+    window.location.hostname.endsWith("vercel.app")
+  ) {
+    return null;
+  }
 
   const src = isDev
     ? "https://va.vercel-scripts.com/v1/script.debug.js"
